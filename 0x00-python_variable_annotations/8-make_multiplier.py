55570265403b1a394cf62f660d47825c5fcbc935
Ano-ly <anomalie23.0@gmail.com>
2024-06-07 04:10:29 +0100

    Task 8

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 """Make-multiplier function"""
+from typing import Callable
 
 
-def make_multiplier(multiplier: float) -> 
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Return a function"""
+
+    def new_multiplier(arg: float):
+        """New_multiplier function"""
+
+        return (multiplier * arg)
+    return (new_multiplier)
