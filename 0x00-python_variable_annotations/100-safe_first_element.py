a5a69f22be04f4b97da9152a70316c92ce787fb1
Ano-ly <anomalie23.0@gmail.com>
2024-06-07 05:10:57 +0100

    Task 100 edit

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -4,6 +4,8 @@ from typing import Any, Union, Sequence
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any,  None]:
+    """Return the firat element of a sequence safely"""
+
     if lst:
         return lst[0]
     else:
