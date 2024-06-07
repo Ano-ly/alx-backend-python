e0e754d2a9f1f6ac46c07352ca70df394b2e2420
Ano-ly <anomalie23.0@gmail.com>
2024-06-07 03:49:50 +0100

    Tasks 6-7 edit

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """Function sum_list"""
-
-
 from typing import List
+
 
 def sum_list(input_list: List[float]) -> float:
     """Takes an input list and returns their sum as a float"""
--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """Type-annotated function to_kv"""
+from typing import Tuple
 
-def to_kv(k: str, v: int | float) -> tuple[str, float]:
+
+def to_kv(k: str, v: int | float) -> Tuple[str, float]:
     """
     Takes a string and an int or float and returns a tuple
     """
 
-    return((k, v ** 2))
+    return ((k, v ** 2))
