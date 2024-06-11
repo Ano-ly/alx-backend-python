7812964c82f42af9b1784fa058b7f959ad45a6fa
Ano-ly <anomalie23.0@gmail.com>
2024-06-11 05:36:19 +0100

    Complete task 3

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
 """Contains async routine wait_n"""
 import asyncio
-from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
-    i: int = 0
-    delays: List[float] = []
-    while i < n:
-        delays.append(await asyncio.create_task(wait_random(max_delay)))
-        i += 1
-    return (delays)
+def task_wait_random(max_delay: int):
+    """Create an asyncio task"""
+
+    return (asyncio.create_task(wait_random(max_delay)))
