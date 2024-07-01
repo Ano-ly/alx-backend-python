4311281b15e33592064c063c3172fffbf8009dfb
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-07-01 09:22:46 +0100

    Task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -16,6 +16,15 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test access_nested map function"""
         self.assertEqual(a_n_m(the_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
+    ])
+    def test_access_nested_map_exception(self, the_map, path, message):
+        """Test that KeyError is raised with the above inputs"""
+        a_n_m(the_map, path)
+        self.assertRaises(KeyError, message)
+
 
 if __name__ == "__main__":
     unittest.main()
